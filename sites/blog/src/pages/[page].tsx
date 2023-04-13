5aff9e40ce95c47a07eb1b602471cf0af330c43a
Mauricio Robayo <mauricio.robayo@kinandcarta.com>
2023-04-13 09:19:26 -0500

    component mapping

--- sites/blog/src/pages/[page].tsx
+++ sites/blog/src/pages/[page].tsx
@@ -1,21 +1,25 @@
 import { getPageByUrl } from "contentstack";
-import { GetServerSideProps, InferGetServerSidePropsType } from "next";
+import { InferGetServerSidePropsType } from "next";
 import Head from "next/head";
-import { Actions, BucketsProps, FeaturedPosts, Hero, Spotlight } from "ui";
+import { Actions, Buckets, FeaturedPosts, Hero, Spotlight } from "ui";
 
-const mainContentComponents: { [key: string]: any } = {
-  PageMainContentBuckets: BucketsProps,
+const mainContentComponents = {
+  PageMainContentBuckets: Buckets,
   PageMainContentHeroSection: Hero,
   PageMainContentActions: Actions,
   PageMainContentBlog: FeaturedPosts,
   PageMainContentSpotlight: Spotlight,
+  PageMainContentRichText: () => null,
 };
 
 export default function Page({
   page,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return page.content?.map(({ type, content }) => {
-    const Component = mainContentComponents[type!] ?? {};
+    if (!type) {
+      return null;
+    }
+    const Component = mainContentComponents[type] ?? {};
     if (Component) {
       return (
         <>
